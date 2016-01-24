6537d93e4cc242edb6ff1ed95b7a3d90778b5f6b
kskrishnakanth <kskrishnakanth3@gmail.com>
2016-01-24 20:31:39 +0530

    Update compression.c

--- compression.c
+++ compression.c
@@ -11,36 +11,35 @@ int main()
 }
 char* compress(char* str)
 {
-  int i,j,count=1;
+	int i,j,count=1;
 	char comps[150]="",temp1[150],temp2[150];
 	for(i=0;str[i]!='\0';i++)
 	{
-		  j=i+1;
-	    while(str[i]==str[j]&&str[j]!='\0')
-	    {
-	    	count++;
-	    	j++;
-	    }
-	  	sprintf(temp1,"%c",str[i]); //temp variable to store repeated character
-		  i=j-1;
-		  if(count>2)
-		  {
-			  sprintf(temp2,"%d",count); // if count > 2 count is stored
-			  strcat(temp1,temp2);
-	      strcat(comps,temp1);
-		  }
-	    else if(count==2)
-	    {
-	    	sprintf(temp2,"%c",str[i]);   // else character itself is printed
-	    	strcat(temp1,temp2);
-	      strcat(comps,temp1);
-		  }
-		  else
-		  {
-			  strcat(comps,temp1);        // when character is not repeated same character is stored in string
-		  }
-	    count=1;
-  }
-	
-  return comps;
+		j=i+1;
+		while(str[i]==str[j]&&str[j]!='\0')
+		{
+			count++;
+			j++;
+		}
+		sprintf(temp1,"%c",str[i]); //temp variable to store repeated character
+		i=j-1;
+		if(count>2)
+		{
+			sprintf(temp2,"%d",count); // if count > 2 count is stored
+			strcat(temp1,temp2);
+			strcat(comps,temp1);
+		}
+		else if(count==2)
+		{
+			sprintf(temp2,"%c",str[i]);   // else character itself is printed
+		    	strcat(temp1,temp2);
+			strcat(comps,temp1);
+		}
+		else
+		{
+			strcat(comps,temp1);        // when character is not repeated same character is stored in string
+		}
+		count=1;
+	}
+	return comps;
 }
